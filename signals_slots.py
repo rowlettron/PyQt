96b79505c9e98003c18bc481da57364eff03ce0e
rowlettron <rowlettron@yahoo.com>
2022-09-10 12:28:24 -0500

    Initial creation of signal_slots.py and pycalc.py

--- signals_slots.py
+++ signals_slots.py
@@ -23,3 +23,13 @@ window = QWidget()
 window.setWindowTitle("Signals and Slots")
 layout = QVBoxLayout()
 
+button = QPushButton("Greet")
+button.clicked.connect(greet)
+
+layout.addWidget(button)
+msgLabel = QLabel()
+layout.addWidget(msgLabel)
+window.setLayout(layout)
+window.show()
+sys.exit(app.exec())
+
